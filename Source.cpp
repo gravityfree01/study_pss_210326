735472f42b4f3e6637fdcb4ec56470eaaf175c52
DESKTOP-BHJPBEP\gravi <gravityfree01@gmail.com>
2021-03-26 21:46:40 +0900

    과제1

---
+++ Source.cpp
@@ -0,0 +1,32 @@
+/*
+
+	1.
+	첫번째 값을 입력해주세요 >> 입력1
+	두번째 값을 입력해주세요 >> 입력2
+
+
+	2값을 받아서 덧셈 곱셈 나눗셈 뺄셈을
+
+
+	입력1 + 입력2 = 결과값
+	입력1 * 입력2 = 결과값
+	입력1 / 입력2 = 결과값
+	입력1 - 입력2 = 결과값
+
+
+*/
+
+#include<iostream>
+
+using namespace std;
+
+void main() {
+
+	int a = 10, b = 30;
+
+	cout << a + b << endl;
+	cout << a * b << endl;
+	cout << a / b << endl;
+	cout << a - b << endl;
+}
+
